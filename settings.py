b566b79ddec6415dbf00335f93410452c04ee8df
Geodeziy <geodez_y@bk.ru>
2023-04-23 17:06:15 +0300

    Добавление settings.py.

---
+++ settings.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "BOT_TOKEN"  # токен бота
+UNSPLASH_TOKEN = "UNSPLASH_TOKEN"  # ключ API для получения картинок
+RapidAPI_Key = "RapidAPI_Key"  # ключ API для доступа к переводчику Microsoft
+ID = 1234567890  # id пользователя Telegram, для доступа к запуску функции /restart
